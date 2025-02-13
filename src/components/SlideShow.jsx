9dfc9074a44abdb0c4e7dc22ea5faf9396ce45ed
enoamaore14120 <121623851+enoamaore14120@users.noreply.github.com>
2025-02-13 19:33:40 +0100

    Kasa avec slidhsow et boutton sans défillement

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import HouseListing from '../assets/logements.json'
 import '../styles/SlideShow.css'
+import arrowLeft from '../assets/arrow_back_ios-24px 1.png'
+import arrowRight from '../assets/arrow_forward_ios-24px 1.png'
 function SlideShow () {
     const pictures = HouseListing.flatMap((id) => id.pictures)
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,8 +30,8 @@ function SlideShow () {
         className= 'houseSlideShow'/>
         
         <div className="arrow__links">
-            <button onClick={PrevSlide}><img src='../assets/arrow_back_ios-24px 1.png' alt="arrow left" className="arrowLeft" /></button>
-            <button onClick={NextSlide}><img src='../assets/arrow_forward_ios-24px 1.png' alt="arrow Right" className="arrowRight" /></button>
+            <button onClick={PrevSlide}><img src={arrowLeft }alt="arrow left" className="arrowLeft" /></button>
+            <button onClick={NextSlide}><img src={arrowRight}alt="arrow Right" className="arrowRight" /></button>
            
         </div>
     </nav>
