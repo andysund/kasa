1e4827e790ead74792f8ec9f6561d15948ea0cab
enoamaore14120 <121623851+enoamaore14120@users.noreply.github.com>
2025-02-14 16:08:17 +0100

    kasa V2.2

--- src/components/OwnerRatings.jsx
+++ src/components/OwnerRatings.jsx
@@ -4,23 +4,30 @@ import starRed from '../assets/star-red.png'
 import starGrey from '../assets/star-grey.png'
 
 function OwnerRatings({ host: { name, picture }, rating }) {
-  // Conversion de la note en nombre
+  // 1. On sépare le prénom et le nom
+  const fullName = name.trim().split(/\s+/)
+  const lastName = fullName[fullName.length - 1]
+  const firstName = fullName.slice(0, fullName.length - 1).join(' ')
+
+  // 2. Convertir la note en nombre pour l'affichage des étoiles
   const ratingValue = parseInt(rating, 10)
 
   return (
-    <>
-    <div className='owner-name-ratings'>
+    <div className="owner-name-ratings">
       <div className="loft-owner">
+        {/* 3. Afficher le prénom et le nom sur 2 lignes */}
         <div className="loft-owner__name">
-          <h1>{name}</h1>
+          <div>{firstName}</div>
+          <div>{lastName}</div>
         </div>
+
         <div className="loft-owner__picture">
           <img src={picture} alt={name} />
         </div>
       </div>
 
-      {/* Affichage du rating sous forme d'étoiles */}
-      <div className='owner-ratings'>
+      {/* 4. Affichage du rating sous forme d'étoiles */}
+      <div className="owner-ratings">
         {[1, 2, 3, 4, 5].map((index) => (
           <img
             key={index}
@@ -30,7 +37,6 @@ function OwnerRatings({ host: { name, picture }, rating }) {
         ))}
       </div>
     </div>
-    </>
   )
 }
 
